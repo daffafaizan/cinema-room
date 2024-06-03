e8437b2647a5aec938b920b2c859b6f18fe281db
Daffa <daffamfzn@gmail.com>
2024-06-03 10:45:18 +0700

    INTERNDEMO-45 [RED] Create test for get seats endpoint

--- Cinema Room REST Service (Kotlin)/Cinema Room REST Service (Kotlin)/task/test/cinema/controller/CinemaControllerTests.kt
+++ Cinema Room REST Service (Kotlin)/Cinema Room REST Service (Kotlin)/task/test/cinema/controller/CinemaControllerTests.kt
@@ -1,21 +1,26 @@
 package cinema.controller
 
+import cinema.dto.response.CinemaResponseDTO
 import cinema.service.CinemaService
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
-class CinemaControllerTests(@Autowired val mockMvc: MockMvc) {
+class CinemaControllerTests(@Autowired val mockMvc: MockMvc, @Autowired val mapper: ObjectMapper) {
 
     @MockBean
     private lateinit var cinemaService: CinemaService
 
     @Test
-    fun testGetSeatsShouldReturn_WithStatus200() {
+    fun testGetSeatsShouldReturnWithStatus200() {
     }
 }
