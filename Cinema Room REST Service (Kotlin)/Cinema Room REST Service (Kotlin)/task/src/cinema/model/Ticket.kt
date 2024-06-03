446905fc9e3c8c03480a0dcb5eefb047ef159da6
Daffa <daffamfzn@gmail.com>
2024-06-03 14:44:59 +0700

    INTERNDEMO-45 [GREEN] Implemented tests for save, get ticket with invalid token, get with valid token, and update ticket

--- Cinema Room REST Service (Kotlin)/Cinema Room REST Service (Kotlin)/task/src/cinema/model/Ticket.kt
+++ Cinema Room REST Service (Kotlin)/Cinema Room REST Service (Kotlin)/task/src/cinema/model/Ticket.kt
@@ -6,5 +6,5 @@ import java.util.UUID
 data class Ticket(
     val token: String = UUID.randomUUID().toString(),
     @JsonProperty("ticket")
-    val seat: Seat
+    var seat: Seat
 )
---
+++ Cinema Room REST Service (Kotlin)/Cinema Room REST Service (Kotlin)/task/test/cinema/repository/TicketRepositoryTests.kt
@@ -0,0 +1,78 @@
+package cinema.repository
+
+import cinema.model.Seat
+import cinema.model.Ticket
+import cinema.utils.CinemaHelpers
+import cinema.utils.CinemaRepositoryConstants
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketRepositoryTests {
+
+    private lateinit var ticketRepository: TicketRepository
+    private lateinit var seat: Seat
+    private lateinit var ticket: Ticket
+
+    @BeforeEach
+    fun setUp() {
+        ticketRepository = TicketRepository()
+
+        seat = Seat(
+            CinemaRepositoryConstants.ROW,
+            CinemaRepositoryConstants.COLUMN,
+            CinemaHelpers.seatPrice(CinemaRepositoryConstants.ROW, CinemaRepositoryConstants.COLUMN),
+            false
+        )
+        ticket = Ticket(seat=seat)
+
+        ticketRepository.saveTicket(ticket)
+    }
+
+    @Test
+    fun testSaveTicket() {
+        val newTicket = Ticket(seat=seat)
+        ticketRepository.saveTicket(newTicket)
+
+        val actualTicket = ticketRepository.getTicket(newTicket.token)
+        assertEquals(newTicket, actualTicket)
+    }
+
+    @Test
+    fun testGetTicketInvalidToken() {
+        val ticket = ticketRepository.getTicket("INVALID-TOKEN")
+
+        assertTrue(ticket == null)
+    }
+
+    @Test
+    fun testGetTicketValidToken() {
+        val actualTicket = ticketRepository.getTicket(ticket.token)
+        assertEquals(ticket, actualTicket)
+    }
+
+    @Test
+    fun testUpdateTicket() {
+        val seat = Seat(
+            CinemaRepositoryConstants.ROW,
+            CinemaRepositoryConstants.COLUMN,
+            10000,
+            true
+        )
+        val ticket = ticketRepository.getTicket(ticket.token)
+        if (ticket != null) {
+            ticket.seat = seat
+
+            ticketRepository.updateTicket(ticket.token, ticket)
+
+            val actualTicket = ticketRepository.getTicket(ticket.token)
+            assertEquals(ticket, actualTicket)
+            if (actualTicket != null) {
+                assertEquals(seat, actualTicket.seat)
+            }
+        }
+
+    }
+}
